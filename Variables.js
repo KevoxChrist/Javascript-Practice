a16c58b6ceb2108d4021f0af71184a0c51134c34
Kev <105522526+KevoxChrist@users.noreply.github.com>
2025-07-11 15:55:09 -0400

    Create Variables.js

---
+++ Variables.js
@@ -0,0 +1,32 @@
+
+
+//Numbers
+//let age = 25;
+//let price = 10.99;
+//let gpa = 3.8;
+
+/*console.log(`You are ${age} years old`);
+console.log(`The price of this medium pizza is: ${price}`);
+console.log(`Your semester's gpa is a ${gpa}`);
+console.log(typeof age);*/
+
+
+//Strings
+//let first_name = "Peter";
+//let favorite_food = "pizza";
+//let email = "PeterParker@gmail.com";
+
+/*console.log(`Your first name is ${first_name}`);
+console.log(`You like ${favorite_food}`);
+console.log(`Your email is: ${email}`);
+console.log(typeof first_name);*/
+
+//BOOLEANS
+let online = true;
+let for_sale = false;
+let is_student = true;
+
+console.log(`Bro is online: ${online}`);
+console.log(`Minecraft is on sale for $15: ${for_sale}`);
+console.log(`Enrollment Status: ${is_student}`);
+console.log(typeof is_student);
